c8ce4bd1ad18b97e3c0f953588b7c05584f8966e
Balthazar GRONON <bgronon@gmail.com>
2013-12-14 23:31:38 +0100

    Improving regex to prevent bypass of some shitters

--- js/content.js
+++ js/content.js
@@ -3,13 +3,14 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
 	if (message.type == "toggle_shit")
 	{
 		var shitUsers = [
-			"grebett"
+			"qperez",
+			"tlopes-g"
 		];
 
 		$('li.item').each(function () {
 			var href = $(this).find('a').attr('href');
 			if (href) {
-				var reg = href.match(/user\/([a-z]*)\//);
+				var reg = href.match(/user\/([a-z]+-*[a-z]*)\//);
 				if (reg && reg[1] && shitUsers.indexOf(reg[1]) != -1) {
 					$(this).toggle();	
 				}
@@ -17,7 +18,7 @@ chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
 		});
 
 		$('span.info').each(function () {
-			var reg = $(this).html().match(/— ([a-z]*)/);
+			var reg = $(this).html().match(/— ([a-z]+-*[a-z]*)/);
 			if (reg && reg[1] && shitUsers.indexOf(reg[1]) != -1)
 				$(this).parentsUntil(".item").toggle();
 		});
