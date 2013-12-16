25b719d6fdcc5554eae3d610be8b5d751b0e6e20
Balthazar GRONON <bgronon@gmail.com>
2013-12-16 22:42:12 +0100

    Total count working, current buggy

--- js/background.js
+++ js/background.js
@@ -1,37 +1,50 @@
 var config;
+var count;
 
+/*
+** Load of the config, init badge
+*/
 chrome.storage.sync.get('ShitBlockConfig', function(result){
-	if (!isEmpty(result))
-		config = result.ShitBlockConfig;
-	else
-		config = { blocked : {}, enabled : true };
-
-	if (config.enabled == false)
-	{
-		chrome.browserAction.setBadgeText({text: "0"});
-		chrome.browserAction.setBadgeBackgroundColor({color: "#CC0000"});
-	}
-	else
-	{
-		chrome.browserAction.setBadgeText({text: "0"});
-		chrome.browserAction.setBadgeBackgroundColor({color: "#009900"});
-	}
+	config = (!isEmpty(result)) ? result.ShitBlockConfig : { blocked : {}, enabled : true };
+	chrome.browserAction.setBadgeText({text: "" });
+	chrome.browserAction.setBadgeBackgroundColor({color: "#009900"});
+});
+chrome.storage.sync.get('ShitBlockCount', function(result){
+	count = (!isEmpty(result)) ? result.ShitBlockCount : { current : 0, total : 0 };
 });
 
+/*
+** Listener on change config
+*/
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-	config = changes["ShitBlockConfig"].newValue;
-	if (config.enabled == false)
+	if (changes["ShitBlockConfig"])
 	{
-		chrome.browserAction.setBadgeText({text: "0"});
-		chrome.browserAction.setBadgeBackgroundColor({color: "#CC0000"});
-	}
-	else
-	{
-		chrome.browserAction.setBadgeText({text: "0"});
-		chrome.browserAction.setBadgeBackgroundColor({color: "#009900"});
+		config = changes["ShitBlockConfig"].newValue;
+		if (config.enabled == false)
+			chrome.browserAction.setBadgeText({text: "" });
 	}
 });
 
+/*
+** Listener for update count and delete count when not on post
+*/
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+	chrome.browserAction.setBadgeText({text: "0" });
+	if (request.type == "incrementCount" && config.enabled == true)
+	{
+		chrome.browserAction.setBadgeText({text: request.data.toString() });
+		count.total += request.data;
+		chrome.storage.sync.set({'ShitBlockCount': count });
+		console.log(count.total);
+	}
+	/*else if (request.type == "deleteBadge")
+		chrome.browserAction.setBadgeText({text: request.data.toString() });*/
+	return true;
+});
+
+/*
+** Test object empty
+*/
 function isEmpty(obj) {
 	for(var prop in obj) {
 		if(obj.hasOwnProperty(prop))
@@ -39,3 +52,14 @@ function isEmpty(obj) {
 	}
 	return true;
 }
+
+
+/*
+Using different tabs,
+But not working : phoque
+
+chrome.tabs.getSelected(null, function(tab){
+	chrome.browserAction.setBadgeText({text: "", tabId:tab.id });
+});
+
+*/
--- js/content.js
+++ js/content.js
@@ -1,25 +1,45 @@
 var config;
 
+/*
+** Get config
+*/
 chrome.storage.sync.get('ShitBlockConfig', function(result){
-	if (!isEmpty(result))
-		config = result.ShitBlockConfig;
-	else
-		config = { blocked : {}, enabled : true };
+	config = (!isEmpty(result)) ? result.ShitBlockConfig : { blocked : {}, enabled : true };
 });
 
+/*
+** Update config if change
+*/
 chrome.storage.onChanged.addListener(function(changes, namespace) {
-	config = changes["ShitBlockConfig"].newValue;
-	updateBlock();
-});
-
-function nodeInsertedCallback(event) {
-	if (event.relatedNode.nodeName == "FORM"){
+	if (changes["ShitBlockConfig"]) 
+	{
+		config = changes["ShitBlockConfig"].newValue;
 		updateBlock();
 	}
+});
+
+/*
+** Listener FOM insert
+*/
+document.addEventListener('DOMNodeInserted', nodeInsertedCallback);
+
+/*
+** Detect form insert into DOM = comments
+*/
+function nodeInsertedCallback(event) {
+	if (event.relatedNode.nodeName == "FORM")
+		updateBlock();
+	else if (event.relatedNode.lastChild.className == "thread list")
+		updateBlock();
 };
 
+/*
+** Toggling display of comment and post on the page
+*/
 function updateBlock(){
+	var	count;
 
+	count = 0;
 	$('li.item').each(function () {
 		var href = $(this).find('a').attr('href');
 		if (href) {
@@ -28,7 +48,10 @@ function updateBlock(){
 				if (config.enabled == false)
 					$(this).show();
 				else
+				{
 					$(this).hide();
+					count++;
+				}
 			}
 			else if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == false)
 				$(this).show();
@@ -42,16 +65,21 @@ function updateBlock(){
 			if (config.enabled == false)
 				$(this).parentsUntil(".item").show();
 			else
+			{
 				$(this).parentsUntil(".item").hide();
+				count++;
+			}
 		}
 		else if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == false)
 			$(this).parentsUntil(".item").show();
 	});
+
+	chrome.extension.sendMessage({type: "incrementCount", data: count });
 }
 
-document.addEventListener('DOMNodeInserted', nodeInsertedCallback);
-
-
+/*
+** Check if object is empty
+*/
 function isEmpty(obj) {
 	for(var prop in obj) {
 		if(obj.hasOwnProperty(prop))
