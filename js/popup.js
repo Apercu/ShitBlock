15e756315878ada86e915d43b81e15cf152aa8fd
Apercu <bgronon@gmail.com>
2013-12-16 02:51:28 +0100

    Omg but it seems to work !

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,38 @@
-window.onload = function() {
-	console.log("test");
+window.onload = function () {
+
+	var config;
 	var checkbox = document.getElementById("switch");
+
+	chrome.storage.sync.get('ShitBlockConfig', function(result){
+		if (!isEmpty(result))
+			config = result.ShitBlockConfig;
+		else
+			config = { blocked : {}, enabled : true };
+
+		if (config.enabled == false)
+			checkbox.checked = false;
+		else
+			checkbox.checked = true;
+	});
+
 	checkbox.onclick = function() {
-		chrome.extension.sendMessage({
-			type: "toggle_shit"
-		});
+		if (config.enabled == false)
+		{
+			config.enabled = true;
+			chrome.storage.sync.set({"ShitBlockConfig" : config});
+		}
+		else
+		{
+			config.enabled = false;
+			chrome.storage.sync.set({"ShitBlockConfig" : config});
+		}
+	}
+
+	function isEmpty(obj) {
+		for(var prop in obj) {
+			if(obj.hasOwnProperty(prop))
+				return false;
+		}
+		return true;
 	}
 }
