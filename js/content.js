35964e66aa4d75c455321df2d6b512fa3863a81f
Balthazar GRONON <bgronon@gmail.com>
2013-12-16 13:20:24 +0100

    Working on toggle only

--- js/content.js
+++ js/content.js
@@ -1,26 +1,57 @@
-chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
+var config;
 
-	console.log("PUTE");
-	var config = chrome.storage.sync.get("ShitBlockConfig");
-	console.log(config);
+chrome.storage.sync.get('ShitBlockConfig', function(result){
+	if (!isEmpty(result))
+		config = result.ShitBlockConfig;
+	else
+		config = { blocked : {}, enabled : true };
+});
 
-
-	if (message.type == "toggle_shit")
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+	config = changes["ShitBlockConfig"].newValue;
+	if (config.enabled == false)
 	{
 		$('li.item').each(function () {
 			var href = $(this).find('a').attr('href');
 			if (href) {
 				var reg = href.match(/user\/([a-z]+-*[a-z]*)\//);
-				if (reg && reg[1] && shitUsers.indexOf(reg[1]) != -1) {
-					$(this).toggle();	
+				console.log(reg[1]);
+				if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == true) {
+					$(this).show();	
 				}
 			}
 		});
 
 		$('span.info').each(function () {
 			var reg = $(this).html().match(/— ([a-z]+-*[a-z]*)/);
-			if (reg && reg[1] && shitUsers.indexOf(reg[1]) != -1)
-				$(this).parentsUntil(".item").toggle();
+			if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == true)
+				$(this).parentsUntil(".item").hide();
+		});
+	}
+	else
+	{
+		$('li.item').each(function () {
+			var href = $(this).find('a').attr('href');
+			if (href) {
+				var reg = href.match(/user\/([a-z]+-*[a-z]*)\//);
+				if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == true) {
+					$(this).hide();	
+				}
+			}
+		});
+
+		$('span.info').each(function () {
+			var reg = $(this).html().match(/— ([a-z]+-*[a-z]*)/);
+			if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == true)
+				$(this).parentsUntil(".item").hide();
 		});
 	}
 });
+
+function isEmpty(obj) {
+	for(var prop in obj) {
+		if(obj.hasOwnProperty(prop))
+			return false;
+	}
+	return true;
+}
