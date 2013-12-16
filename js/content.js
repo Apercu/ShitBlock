447aaaca6b041486aaca7228a1975f1cf24b9977
Balthazar GRONON <bgronon@gmail.com>
2013-12-16 14:15:22 +0100

    Handle instant update block/unblock

--- js/content.js
+++ js/content.js
@@ -9,44 +9,49 @@ chrome.storage.sync.get('ShitBlockConfig', function(result){
 
 chrome.storage.onChanged.addListener(function(changes, namespace) {
 	config = changes["ShitBlockConfig"].newValue;
-	if (config.enabled == false)
-	{
-		$('li.item').each(function () {
-			var href = $(this).find('a').attr('href');
-			if (href) {
-				var reg = href.match(/user\/([a-z]+-*[a-z]*)\//);
-				console.log(reg[1]);
-				if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == true) {
-					$(this).show();	
-				}
-			}
-		});
-
-		$('span.info').each(function () {
-			var reg = $(this).html().match(/— ([a-z]+-*[a-z]*)/);
-			if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == true)
-				$(this).parentsUntil(".item").hide();
-		});
-	}
-	else
-	{
-		$('li.item').each(function () {
-			var href = $(this).find('a').attr('href');
-			if (href) {
-				var reg = href.match(/user\/([a-z]+-*[a-z]*)\//);
-				if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == true) {
-					$(this).hide();	
-				}
-			}
-		});
-
-		$('span.info').each(function () {
-			var reg = $(this).html().match(/— ([a-z]+-*[a-z]*)/);
-			if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == true)
-				$(this).parentsUntil(".item").hide();
-		});
-	}
+	console.log(config);
+	updateBlock();
 });
+
+function nodeInsertedCallback(event) {
+	if (event.relatedNode.nodeName == "FORM"){
+		updateBlock();
+	}
+};
+
+function updateBlock(){
+
+	$('li.item').each(function () {
+		var href = $(this).find('a').attr('href');
+		if (href) {
+			var reg = href.match(/user\/([a-z]+-*[a-z]*)\//);
+			if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == true) {
+				if (config.enabled == false)
+					$(this).show();
+				else
+					$(this).hide();
+			}
+			else if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == false)
+				$(this).show();
+		}
+	});
+
+	$('span.info').each(function () {
+		var reg = $(this).html().match(/— ([a-z]+-*[a-z]*)/);
+		if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == true)
+		{
+			if (config.enabled == false)
+				$(this).parentsUntil(".item").show();
+			else
+				$(this).parentsUntil(".item").hide();
+		}
+		else if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == false)
+			$(this).parentsUntil(".item").show();
+	});
+}
+
+document.addEventListener('DOMNodeInserted', nodeInsertedCallback);
+
 
 function isEmpty(obj) {
 	for(var prop in obj) {
