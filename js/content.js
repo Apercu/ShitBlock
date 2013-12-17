31bd9e3f19ab77b36046d487cfd8ffdcd75a7ac7
Balthazar GRONON <bgronon@gmail.com>
2013-12-17 17:36:07 +0100

    fixin array config

--- js/content.js
+++ js/content.js
@@ -1,10 +1,18 @@
-var config;
+var config, blocked;
 
 /*
 ** Get config
 */
 chrome.storage.sync.get('ShitBlockConfig', function(result){
 	config = (!isEmpty(result)) ? result.ShitBlockConfig : { blocked : [], enabled : true };
+
+	blocked = (function (tab) {
+		var out = [];
+		for (var i = 0; i < tab.length; i++) {
+			out.push(tab[i].login);
+		}
+		return (out);
+	})(config.blocked);
 });
 
 /*
@@ -33,6 +41,7 @@ function nodeInsertedCallback(event) {
 		updateBlock();
 };
 
+
 /*
 ** Toggling display of comment and post on the page
 */
@@ -44,7 +53,7 @@ function updateBlock(){
 		var href = $(this).find('a').attr('href');
 		if (href) {
 			var reg = href.match(/user\/([a-z]+-*[a-z]*)\//);
-			if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == true) {
+			if (reg && reg[1] && blocked.indexOf(reg[1]) != -1) {
 				if (config.enabled == false)
 					$(this).show();
 				else
@@ -53,14 +62,14 @@ function updateBlock(){
 					count++;
 				}
 			}
-			else if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == false)
+			else if (reg && reg[1] && blocked.indexOf(reg[1]) != -1)
 				$(this).show();
 		}
 	});
 
 	$('span.info').each(function () {
 		var reg = $(this).html().match(/— ([a-z]+-*[a-z]*)/);
-		if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == true)
+		if (reg && reg[1] && blocked.indexOf(reg[1]) != -1)
 		{
 			if (config.enabled == false)
 				$(this).parentsUntil(".item").show();
@@ -70,22 +79,11 @@ function updateBlock(){
 				count++;
 			}
 		}
-		else if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == false)
+		else if (reg && reg[1] && blocked.indexOf(reg[1]) != -1)
 			$(this).parentsUntil(".item").show();
 	});
 
 	chrome.extension.sendMessage({type: "incrementCount", data: count });
-}
-
-/*
-** Check if object is empty
-*/
-function isEmpty(obj) {
-	for(var prop in obj) {
-		if(obj.hasOwnProperty(prop))
-			return false;
-	}
-	return true;
 }
 
 /*
@@ -100,7 +98,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 		var href = $(this).find('a').attr('href');
 		if (href) {
 			var reg = href.match(/user\/([a-z]+-*[a-z]*)\//);
-			if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == true) {
+			if (reg && reg[1] && blocked.indexOf(reg[1]) != -1) {
 				if (config.enabled == true)
 					count++;
 			}
@@ -109,7 +107,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
 	$('span.info').each(function () {
 		var reg = $(this).html().match(/— ([a-z]+-*[a-z]*)/);
-		if (reg && reg[1] && config.blocked.hasOwnProperty(reg[1]) == true)
+		if (reg && reg[1] && blocked.indexOf(reg[1]) != -1)
 		{
 			if (config.enabled == true)
 				count++;
@@ -117,3 +115,14 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	});
 	sendResponse({countOnTab: count});
 });
+
+/*
+** Check if object is empty
+*/
+function isEmpty(obj) {
+	for(var prop in obj) {
+		if(obj.hasOwnProperty(prop))
+			return false;
+	}
+	return true;
+}
